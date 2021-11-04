005f3c942f0236f591899c3575eca0c0448ef272
rigyeonghong <zxcvbee@naver.com>
2021-11-05 03:12:56 +0900

    정답

--- 17298.py
+++ 17298.py
@@ -4,13 +4,13 @@ n = int(input())
 inputValue = list(map(int, input().split()))
 
 oh_big = [-1] * n
-stack = deque()
+que = deque()
 
 for i in range(n):
-    while stack and (stack[-1][0] < inputValue[i]):
-        tmp, idx = stack.pop()
+    while que and (que[-1][0] < inputValue[i]):
+        tmp, idx = que.pop()
         oh_big[idx] = inputValue[i]
-    stack.append([inputValue[i],i])
+    que.append([inputValue[i],i])
 
 print(*oh_big)
 
