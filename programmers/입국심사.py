bee8decbd1ca3968cc2ec5ea9d39431bb8f036ed
rigyeonghong <zxcvbee@naver.com>
2022-01-27 23:51:28 +0900

    이분탐색

---
+++ programmers/입국심사.py
@@ -0,0 +1,22 @@
+import sys
+ 
+N, M = map(int, sys.stdin.readline.split())
+time = [int(input().rstrip()) for _ in range(N)]
+ 
+result = 0
+left = 0
+right = M*(min(time))
+ 
+while left <= right:
+    mid = (left+right)//2
+ 
+    judgePeople = 0
+    for t in time:
+        judgePeople += mid//t
+    
+    if judgePeople < M:
+        left = mid+1
+    else:
+        result = mid
+        right = mid-1
+print(result)
