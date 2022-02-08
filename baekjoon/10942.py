2e620050515a986e649f76faf6e0cc5e255c6187
rigyeonghong <zxcvbee@naver.com>
2022-02-08 22:38:34 +0900

    DP

--- baekjoon/10942.py
+++ baekjoon/10942.py
@@ -1,18 +1,23 @@
 import sys
+from typing import List
+input = sys.stdin.readline
 
-N = int(sys.stdin.readline())
-data = list(map(int, sys.stdin.readline().split()))
-M = int(sys.stdin.readline())
-# Q = []
-for _ in range(M):
-    # Q.append(list(map(int, sys.stdin.readline().split())))
-    Q = list(map(int, sys.stdin.readline().split()))
-    start = Q[0]
-    end = Q[1]
+n = int(input())
+data = list(map(int,input().split()))
+m = int(input())
+dp = [[0] * (n+1) for _ in range(n+1)]
 
-    # data[start] ~ data[end]
-    tmp = data[start-1:end]
-    if tmp == tmp[::-1]:
-        print(1)
-    else:
-        print(0)    
+for number in range(n):
+    for s in range(n-number):
+        e = s + number
+        if s == e :
+            dp[s][e] = 1
+        elif data[s] == data[e]:
+            if s + 1 == e :
+                dp[s][e] = 1
+            elif dp[s+1][e-1] == 1:
+                dp[s][e] = 1
+
+for _ in range(m):
+    start,end = map(int,input().split())
+    print(dp[start-1][end-1])
