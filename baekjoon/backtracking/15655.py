0c76a86f9eaf47a19fda5140b0bdefeaa2b6e804
rigyeonghong <zxcvbee@naver.com>
2022-05-12 12:07:41 +0900

    backtracking5

---
+++ baekjoon/backtracking/15655.py
@@ -0,0 +1,21 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+data = sorted(list(map(int, sys.stdin.readline().split())))
+
+pick = []
+
+def find(i, depth):
+    if depth == M:
+        print(*pick)
+        return
+    
+    for j in range(i+1, N):
+        pick.append(data[j])
+        find(j, depth+1)
+        pick.pop()
+
+for i in range(N):
+    pick.append(data[i])
+    find(i, 1)
+    pick.pop()
