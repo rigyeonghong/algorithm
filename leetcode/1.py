24580a81f0a41d784445fd519daa663964713b16
rigyeonghong <zxcvbee@naver.com>
2022-05-15 17:51:45 +0900

    array, Hash table

--- leetcode/1.py
+++ leetcode/1.py
@@ -1,1 +1,15 @@
-import sys
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        data = dict()
+        
+        for index, value in enumerate(nums):
+            if target - value in data:
+                return [data[target - value], index]
+            else:
+                data[value] = index 
+
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     for i in range(len(nums)):
+    #         for j in range(i+1, len(nums)):
+    #             if nums[i] + nums[j] == target:
+    #                 return [i, j]
