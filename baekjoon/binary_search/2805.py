c53da1d85502c783f42950fcdc9c46accd18e8da
rigyeonghong <zxcvbee@naver.com>
2022-06-08 16:20:27 +0900

    refactor : binary_search 3

--- baekjoon/binary_search/2805.py
+++ baekjoon/binary_search/2805.py
@@ -1,20 +1,21 @@
 import sys
-n, m = map(int, sys.stdin.readline().split())
-tree = list(map(int, sys.stdin.readline().split()))
+from collections import Counter
 
-start, end = 1, max(tree) 
+n, m = map(int, sys.stdin.readline().split())
+tree = Counter(map(int, sys.stdin.readline().split()))
+start, end = 0, max(tree) 
 
 while start <= end:
     mid = (start + end) // 2
 
     total_tree = 0
-    for t in tree:
-        if t >= mid:
-            total_tree += t - mid
+    for i,j in tree.items():
+        if i > mid:
+            total_tree += (i-mid)*j
 
     if total_tree >= m:
         start = mid + 1
     else:
         end = mid - 1
-    
+
 print(end)
