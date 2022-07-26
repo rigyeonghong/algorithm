3af45fab79ed7aa4f4564120825e2878d0693085
rigyeonghong <zxcvbee@naver.com>
2022-07-26 14:44:10 +0900

    One-pass Hash Table

--- leetcode/1.py
+++ leetcode/1.py
@@ -1,15 +1,10 @@
+#O(N)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        data = dict()
-        
-        for index, value in enumerate(nums):
-            if target - value in data:
-                return [data[target - value], index]
+        hashmap = {}
+        for idx, value in enumerate(nums):
+            tmp = target - value
+            if tmp in hashmap:
+                return [hashmap[tmp], idx]
             else:
-                data[value] = index 
-
-    # def twoSum(self, nums: List[int], target: int) -> List[int]:
-    #     for i in range(len(nums)):
-    #         for j in range(i+1, len(nums)):
-    #             if nums[i] + nums[j] == target:
-    #                 return [i, j]
+                hashmap[value] = idx
